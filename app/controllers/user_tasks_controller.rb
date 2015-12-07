0e8e5d3ebc2af70cafdcea7ab7475c442a3a5142
Aline <alineleaadams@gmail.com>
2015-12-07 08:42:25 -0600

    associated tasks with users

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,4 +1,5 @@
 class UserTasksController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_task
   before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
 
@@ -25,6 +26,7 @@ class UserTasksController < ApplicationController
   # POST /user_tasks.json
   def create
     @user_task = UserTask.new(user_task_params)
+    @user_task.user_id = current_user.id
 
     respond_to do |format|
       if @user_task.save
@@ -67,12 +69,22 @@ class UserTasksController < ApplicationController
 
   private
     def all_tasks
-      @user_tasks = UserTask.order(:due)
+      @user_tasks = UserTask.where(user_id: current_user.id).order(:due)
     end
 
     # Use callbacks to share common setup or constraints between actions.
     def set_user_task
       @user_task = UserTask.find(params[:id])
+      if current_user.id = @user_task.user_id
+        @user_task
+      else
+        invalid_task
+      end
+
+      def invalid_task
+        logger.error "Attempt to access invalid task #{params[:id]}."
+        redirect_to user_tasks_url, notice: 'Invalid task'
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :user_tasks
 
 # create user using the hash that's returned from the facebook API
   def self.create_with_omniauth(auth)
